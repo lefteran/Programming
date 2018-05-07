c274bb369b045ffe8e34f2d96c7e296060e8068f
lefteran <lefterans@gmail.com>
2018-05-07 11:54:52 +0100

    exercises 7 and 8

---
+++ coursera/machine learning/some commands.m
@@ -0,0 +1,17 @@
+a=[1 2 3; 4 5 6; 7 8 9; 10 11 12;]
+
+
+a(2,:)	%second row
+
+a(:,2) % second column
+
+a(:,2:end) %all columns from the second till the last column
+
+max(A, [], 2) % max value of each row of matrix A
+
+
+the_index=find(ismember(vocabList, str));			% get a vecor of the indices of the elements who belong to vocabList
+
+find(idx==2)		% get a vector of indices with non zero value
+
+%https://wiki.octave.org/Tips_and_tricks
